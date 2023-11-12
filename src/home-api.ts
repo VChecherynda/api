b60d6c81c20ee9021c0cdea4b00608f23b38c2fb
vchecherynda <vchecherynda@shutterstock.com>
2023-11-12 11:14:30 +0200

    Init commit

---
+++ src/home-api.ts
@@ -0,0 +1,6 @@
+import "./set-public-path";
+
+// Anything exported from this file is importable by other in-browser modules.
+export function publicApiFunction() {
+  console.log("[publicApiFunction]");
+}
